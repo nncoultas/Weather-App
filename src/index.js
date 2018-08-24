d49758b83430f0631cca7b7e7af185acc06dffd5
Nick Coultas <nncoultas@yahoo.com>
2018-08-23 19:01:09 -0700

    Removed Redux dev  tools extension package

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers/index';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = applyMiddleware(ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={store(rootReducer, composeWithDevTools())}>
+  <Provider store={store(rootReducer)}>
     <App />
   </Provider>,
   document.getElementById('root')
